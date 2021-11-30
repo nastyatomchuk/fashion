acf940c980ee747a78b6509e28a1e91e26e3acba
NastiushaTom <78820851+NastiushaTom@users.noreply.github.com>
2021-11-30 15:37:08 +0300

    set up Capistrano

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  server "157.245.47.160", user: "deployer", roles: %w{app db web}, primary: true
+
 end
