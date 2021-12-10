8608db9e7700a68659bdf4d42fecbd5137b0191d
nastyatomchuk <nastiusha_tom@icloud.com>
2021-12-10 23:07:43 +0300

    welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/i'
+  root "welcome#i"
+
+  get 'welcome', to: "welcome#i"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
