dbd9c5cad0bc73c715c33616a5dae941445114ec
Anastasiya Tomchuk <anastasiya@wsb-030-72b.local>
2021-12-21 16:45:47 +0300

    add some styles

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get 'persons/profile', as: 'user_root'
 
+  get 'admin' => 'welcome#i'
+
   get 'admin/users' => 'users#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
