d8cf2eb869d80445f4f369bad93462316c9a87fa
Anastasiya Tomchuk <anastasiya@wsb-030-72b.local>
2021-12-24 16:15:18 +0300

    added helpers fot user validation

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,17 @@
 module UsersHelper
-  
+  def field_validity_class(model, attribute)
+    return unless model.errors.include?(attribute)
+
+      "is-invalid"
+
+  end
+
+  def user_valiadation(model, attribute)
+    return unless  model.errors.include?(attribute)
+    <div class="invalid-feedback">
+      model.errors.full_messages_for(attribute).each do |error_message|
+          error_message
+      end
+    </div>
+    end
 end
