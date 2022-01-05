dba1bfe97dd20e8f93ef7ee7c2f9a68c3734588e
Anastasiya Tomchuk <anastasiya@wsb-030-72b.local>
2022-01-05 12:21:03 +0300

    fix

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,16 @@
 module ProductsHelper
+  def field_validity_class(model, attribute)
+    return unless model.errors.include?(attribute)
+
+    "is-invalid"
+
+  end
+
+  def filed_errors(model, attribute)
+    return unless model.errors.include?(attribute)
+
+    content_tag :div, :class => "invalid-feedback" do
+      model.errors.full_messages_for(attribute).join(', ')
+    end
+  end
 end
