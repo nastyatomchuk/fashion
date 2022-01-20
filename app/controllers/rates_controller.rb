3e4d7c02fc32679f78ed71c6febf81a7e35ea61c
nastyatomchuk <nastiusha_tom@icloud.com>
2022-01-20 17:03:59 +0300

    fix

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,4 +1,6 @@
 class RatesController < ApplicationController
+  before_action :set_rate, only: %i[ show edit update destroy ]
+
   def index
     @rate = Rate.all
   end
@@ -8,22 +10,42 @@ class RatesController < ApplicationController
     @currencies = Currency.all
   end
 
+  def show
+  end
+
   def create
-    @rate = Rate.new(product_params)
+    @rate = Rate.new(rate_params)
 
     if @rate.save
-      redirect_to @rate
+      redirect_to rates_path
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @rate.update(rate_params)
+      redirect_to rates_path, notice: "Rate was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity 
+    end
+  end
+
+  def destroy
+    @rate.destroy
+    redirect_to rates_url, notice: "Rate was successfully destroyed."
+  end
+
+
   private
-  def set_product
+  def set_rate
     @rate = Rate.find(params[:id])
   end
 
-  def product_params
-    params.require(:rate).permit(:rate, :currencies_id)
+  def rate_params
+    params.require(:rate).permit(:rate, :currency_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :products
-  resources :users
-  resources :currency
-  resources :rates
 
   get 'admin', to: 'welcome#i'
 
