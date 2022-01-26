b47c5b1831ecbee849e92a2d1e51d45aa519089c
Nastya Tomchuk <nastiusha_tom@icloud.com>
2022-01-26 13:47:53 +0300

    Added noty

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   root "persons#profile"
 
   devise_for :users
+
+  resources :products
   resources :users
-  
+
+  get  "users/:id" => "users#show"
+
+  get 'admin' => 'welcome#i'
+
+  get 'admin/users' => 'users#index'
 end
