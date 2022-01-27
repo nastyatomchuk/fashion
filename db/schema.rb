c8f3d95dead570031d45572bc39cc0fe44ea9591
Nastya Tomchuk <nastiusha_tom@icloud.com>
2022-01-27 15:54:55 +0300

    Created products table

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,4 @@ ActiveRecord::Schema.define(version: 2022_01_20_152859) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
