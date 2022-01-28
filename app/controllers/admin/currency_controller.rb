d63df65493a4e1c4b51997923e7a81a784a85b9f
Nastya Tomchuk <nastiusha_tom@icloud.com>
2022-01-28 15:45:04 +0300

    fix

--- app/controllers/admin/currency_controller.rb
+++ app/controllers/admin/currency_controller.rb
@@ -3,27 +3,5 @@ module Admin
     def index
       @currency = Currency.order(:entity)
     end
-
-    def new
-      @currency = Currency.new
-    end
-
-    def create
-      @currency = Currency.new(currency_params)
-      if @currency.save
-        redirect_to @currency
-      else
-        render :new
-      end
-    end
-
-    private
-    def load_currency
-      @currency = Currency.find(params[:id])
-    end
-
-    def currency_params
-      params.require(:currency).permit(:entity, :currency, :alphabetic_code)
-    end
   end
 end
