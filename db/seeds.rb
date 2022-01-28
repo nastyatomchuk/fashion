b271fc1208c6ccb29f67c74f34b833a0015e9625
Nastya Tomchuk <nastiusha_tom@icloud.com>
2022-01-28 16:08:36 +0300

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'csv'
 
 csv_text = File.read(Rails.root.join('db', 'data', 'currency.csv'))
@@ -15,6 +7,5 @@ csv.each do |row|
   t.entity = row[0]
   t.currency = row[1]
   t.alphabetic_code = row[2]
-
   t.save
 end
