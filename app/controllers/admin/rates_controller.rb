6af69d2cd3f8028a5cb055ba71e487c346e00169
Nastya Tomchuk <nastiusha_tom@icloud.com>
2022-02-01 12:16:23 +0300

    Added exchange-rates

---
+++ app/controllers/admin/rates_controller.rb
@@ -0,0 +1,49 @@
+module Admin
+  class RatesController < ApplicationController
+    before_action :load_rate, only: %i[ show edit update destroy ]
+
+    def index
+      @rate = Rate.order(:rate)
+    end
+
+    def new
+      @rate = Rate.new
+      @currencies = Currency.order(:entity)
+    end
+
+    def create
+      @rate = Rate.new(rate_params)
+
+      if @rate.save
+        redirect_to admin_rates_path
+      else
+        render :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @rate.update(rate_params)
+        redirect_to admin_rates_path, notice: "Rate was successfully updated."
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @rate.destroy
+      redirect_to admin_rates_path, notice: "Rate was successfully destroyed."
+    end
+
+    private
+    def load_rate
+      @rate = Rate.find(params[:id])
+    end
+
+    def rate_params
+      params.require(:rate).permit(:rate, :currency_id)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     resources :products
     resources :users
     resources :currency
+    resources :rates
   end
 end
