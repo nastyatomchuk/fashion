7cf4d3d406f2764809a6fdd4c0dda624172fa0fa
Anton Ageev <anthony.ageev@gmail.com>
2022-02-02 19:51:40 +0300

    Some beauty added to currency seeds

--- db/seeds/currency.seeds.rb
+++ db/seeds/currency.seeds.rb
@@ -1,10 +1,14 @@
 require 'csv'
 
 unless Currency.exists?(entity: 'BELARUS')
+  puts "💸 Seeding currencies:"
+  puts
   csv_text = File.read(Rails.root.join('db', 'data', 'currency.csv'))
   csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
   csv.each do |row|
     сurrency_attrs = { entity: row[0], currency: row[1], alphabetic_code: row[2] }
-    Currency.create!(сurrency_attrs)
+    currency = Currency.create!(сurrency_attrs)
+    puts "     #{currency.alphabetic_code} (#{currency.currency})"
   end
+  puts
 end
