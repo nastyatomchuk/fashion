e6d9317aabc880672cee711e65502545b33ac8a3
Nastya Tomchuk <nastiusha_tom@icloud.com>
2022-02-02 16:38:35 +0300

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,10 @@
 require 'csv'
 
-csv_text = File.read(Rails.root.join('db', 'data', 'currency.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-csv.each do |row|
-  t = Currency.new
-  t.entity = row[0]
-  t.currency = row[1]
-  t.alphabetic_code = row[2]
-  t.save
+if Currency.exists?(entity: 'BELARUS')
+  csv_text = File.read(Rails.root.join('db', 'data', 'currency.csv'))
+  csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+  csv.each do |row|
+    сurrency_attrs = { entity: row[0], currency: row[1], alphabetic_code: row[2] }
+    Currency.create!(сurrency_attrs)
+  end
 end
