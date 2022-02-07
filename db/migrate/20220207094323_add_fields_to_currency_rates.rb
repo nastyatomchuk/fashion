a615ecd30045e479e4dbdb33771a89520fb227ef
Nastya Tomchuk <nastiusha_tom@icloud.com>
2022-02-07 12:50:01 +0300

    Added fields to CurrencyRate table

---
+++ db/migrate/20220207094323_add_fields_to_currency_rates.rb
@@ -0,0 +1,7 @@
+class AddFieldsToCurrencyRates < ActiveRecord::Migration[6.1]
+  def change
+    add_column(:currency_rates, :source, :string)
+    add_column(:currency_rates, :target, :string)
+    add_column(:currency_rates, :date, :datetime)
+  end
+end
