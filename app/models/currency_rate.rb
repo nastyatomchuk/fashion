6ebf7e016a72f7d0b0440c13d3735898f1c72d9c
Nastya Tomchuk <nastiusha_tom@icloud.com>
2022-02-08 17:49:13 +0300

    fixg

--- app/models/currency_rate.rb
+++ app/models/currency_rate.rb
@@ -1,4 +1,4 @@
 class CurrencyRate < ApplicationRecord
   validates :source, :target, presence: true
-  validates :rate, presence: true, numericality: { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }
+  validates :rate, presence: true, numericality: { :greater_than_or_equal_to => 0 }
 end
