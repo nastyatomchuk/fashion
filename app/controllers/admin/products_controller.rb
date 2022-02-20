e6dd2f098559f6aeb7b3b42ace7f14a2dc8ebb51
nastyatomchuk <nastiusha_tom@icloud.com>
2022-02-21 02:27:27 +0300

    Add admin authorizaion

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,10 +1,13 @@
 module Admin
   class ProductsController < ApplicationController
+    before_action :authenticate_user!
     before_action :load_product, only: %i[ show edit update destroy ]
+
     layout 'admin'
 
     def index
       @products = Product.order(:title)
+      authorize @products
     end
 
     def show
@@ -12,6 +15,7 @@ module Admin
 
     def new
       @product = Product.new
+      authorize @product
     end
 
     def edit
@@ -19,6 +23,7 @@ module Admin
 
     def create
       @product = Product.new(product_params)
+      authorize @product
       if @product.save
         redirect_to admin_products_path, notice: "Product was successfully created."
       else
@@ -27,6 +32,7 @@ module Admin
     end
 
     def update
+      authorize @product
       if @product.update(product_params)
         redirect_to admin_products_path, notice: "Product was successfully updated."
       else
@@ -35,6 +41,7 @@ module Admin
     end
 
     def destroy
+      authorize @product
       @product.destroy
       redirect_to admin_products_path, notice: "Product was successfully destroyed."
     end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,12 @@
 module Admin
   class UsersController < ApplicationController
+    before_action :authenticate_user!
     before_action :load_user, only: %i[show edit update destroy]
     layout 'admin'
 
     def index
       @users = User.order(:full_name)
+      authorize @users
     end
 
     def show
@@ -12,6 +14,7 @@ module Admin
 
     def new
       @user = User.new
+      authorize @user
     end
 
     def edit
@@ -19,6 +22,7 @@ module Admin
 
     def create
       @user = User.new(user_params)
+
       if @user.save
         redirect_to admin_users_path, notice: "User was successfully created."
       else
@@ -28,6 +32,7 @@ module Admin
     end
 
     def update
+      authorize @user
       if @user.update(user_params)
         redirect_to admin_users_path, notice: "User was successfully updated."
       else
@@ -37,6 +42,7 @@ module Admin
     end
 
     def destroy
+      authorize @user
       @user.destroy
       redirect_to admin_user_path, notice: "User was successfully destroyed."
     end
