64926b70303051db63759698bf808e393ae3cdae
nastyatomchuk <nastiusha_tom@icloud.com>
2022-02-21 12:20:52 +0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get 'order/show'
   get 'cart_items/create'
   get 'cart_items/update'
   get 'cart_items/destroy'
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :cart_items
   get 'cart' => 'cart#show'
-  get 'order' => 'order#show'
+  get 'order', to: 'order#show'
 
   get  "users/:id" => "users#show"
   get 'admin' => 'welcome#i'
