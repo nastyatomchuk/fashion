c5099df170a76fb9ac72c543c897a1d552d3e273
Nastya Tomchuk <nastiusha_tom@icloud.com>
2022-02-22 17:25:43 +0300

    Add fields to Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_132528) do
+ActiveRecord::Schema.define(version: 2022_02_22_140716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 2022_02_22_132528) do
     t.decimal "price"
     t.integer "store_qty"
     t.string "images", default: [], array: true
+    t.string "sku"
+    t.float "rate"
   end
 
   create_table "users", force: :cascade do |t|
