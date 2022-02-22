3a87974b6d4a2f1346f077ee767cbb73d82747ff
Nastya Tomchuk <nastiusha_tom@icloud.com>
2022-02-22 13:16:53 +0300

    Add admin styles adn scripts

---
+++ app/javascript/packs/admin.js
@@ -0,0 +1,22 @@
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "bootstrap"
+import Noty from 'noty'
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+$(document).on('turbolinks:load', function(){
+    new Noty({
+        type: $('#flash_message').attr('flash-type'),
+        theme: 'bootstrap-v4',
+        text: $('#flash_message').text()
+    }).show();
+})
+
+$(document).on('turbolinks:load', function(){
+    $('input.datepicker').data({behaviour: "datepicker"}).datepicker();
+});
